55a1a2f586cbd68ee081cec492cba55e797dbdd7
Casey Carroll <aiyakc@gmail.com>
2022-02-27 11:27:21 -0800

    align url to suggestion in remix docs - add new post to admin panel sidebar

--- app/routes/admin.jsx
+++ app/routes/admin.jsx
@@ -2,7 +2,7 @@ import { Outlet, Link, useLoaderData } from "remix";
 
 import { getPosts } from "~/post";
 import adminStyles from "~/styles/admin.css";
-import AdminIndex from "./admin";
+import NewPost from "./admin/new";
 
 export const links = () => {
   return [{ rel: "stylesheet", href: adminStyles }];
@@ -17,16 +17,20 @@ export default function Admin() {
   return (
     <div className="admin">
       <nav>
-        <h1>Admin</h1>
+        <h1>Admin Panel</h1>
         <p>Click on a post to edit.</p>
         <ul>
           {posts.map((post) => (
             <li key={post.slug}>
-              <Link to={post.slug}>
+              <Link to={"edit/" + post.slug}>
                 {post.title}
               </Link>
             </li>
           ))}
+        </ul>
+        <p>Create a New Post</p>
+        <ul>
+          <li><Link to="new">New Post</Link></li>
         </ul>
       </nav>
       <main>
